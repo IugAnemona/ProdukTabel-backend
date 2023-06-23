2d9047107345c4b0caf969cdf2062e7e36a84db7
IugAnemona <guilhermealvescoc@gmail.com>
2023-06-23 02:11:04 -0300

    adicionar cors e headers

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import express from "express";
 import { router } from "./routes";
 
+const cors = require("cors");
+
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.listen(3030, () => console.log("server on"));
